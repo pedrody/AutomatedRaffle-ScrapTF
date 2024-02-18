a03467d78e459d28b7c202d2cd1591a4d7f8d6fa
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2024-02-18 13:13:31 -0300

    feat: adds raffle information collection functionality

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from seleniumbase import Driver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 import pickle
 
 
-def main():
-    driver = Driver(uc=True)
+def scroll_to_bottom(driver):
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    driver.sleep(2)
 
-    driver.get('https://scrap.tf')
+
+def main():
+    url = 'https://scrap.tf'
+    url_raffles = f'{url}/raffles'
+    driver = Driver(uc=True)
+    driver.get(url)
 
     cookies = pickle.load(open('cookies.pkl', 'rb'))
     for cookie in cookies:
@@ -16,10 +24,30 @@ def main():
         except Exception as e:
             print(e)
 
-    driver.get('https://scrap.tf/raffles')
+    driver.get(url_raffles)
 
-    driver.sleep(900)
+    raffles_links = []
 
+    while True:
+        scroll_to_bottom(driver)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        if "That's all, no more!" in soup.get_text():
+            scroll_to_bottom(driver)
+            break
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    raffles_div = soup.find('div', id='raffles-list')
+    if raffles_div:
+        raffles = raffles_div.find_all('div', class_='panel-raffle')
+        raffles_stats = driver.find_element(
+            By.CSS_SELECTOR, '.raffle-list-stat h1').text
+        for raffle in raffles:
+            raffle_link = raffle.find('a')['href']
+            raffles_links.append(url + raffle_link)
+
+        print('Open Raffles Entered:', raffles_stats)
+
+    driver.sleep(9999)
     driver.quit()
 
 
