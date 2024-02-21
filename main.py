c18c8e28c711e82b53ed1627521bab9a8927dcb9
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2024-02-20 23:33:25 -0300

    feat: implements checking for new raffles

--- main.py
+++ main.py
@@ -37,9 +37,58 @@ def scroll_to_bottom(driver):
     driver.sleep(2)
 
 
+def collect_raffle_links(driver):
+    raffles_links = []
+
+    driver.get('https://scrap.tf/raffles')
+
+    while True:
+        scroll_to_bottom(driver)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        if "That's all, no more!" in soup.get_text():
+            scroll_to_bottom(driver)
+            break
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    raffles_div = soup.find('div', id='raffles-list')
+    if raffles_div:
+        raffles = raffles_div.find_all('div', class_='panel-raffle')
+        for raffle in raffles:
+            if not raffle.has_attr('class') or 'raffle-entered' not in raffle['class']:
+                raffle_link = raffle.find('a')['href']
+                raffles_links.append('https://scrap.tf' + raffle_link)
+
+    return raffles_links
+
+
+def enter_raffle(driver, link):
+    driver.get(link)
+
+    try:
+        raffle_name = driver.find_element(
+            By.CSS_SELECTOR, 'h3.subtitle').text
+
+        raffle_enter_button = driver.find_element(
+            By.XPATH, "(//button[contains(text(),'Enter Raffle')])[2]")
+        driver.execute_script(
+            "arguments[0].click();", raffle_enter_button)
+        print(
+            f'{Color.GREEN}[+] Successfully entered the raffle!{Color.RESET}')
+    except Exception as e:
+        print(
+            f'{Color.RED}[-] Failed to enter the raffle: {e}{Color.RESET}')
+
+    print(f'    {Color.YELLOW}> Raffle:{Color.RESET} '
+          f'{raffle_name}')
+    print(f'    {Color.YELLOW}> Link:{Color.RESET} {link}')
+
+    print(f'    {Color.YELLOW}> Going to next...\n{Color.RESET}')
+    print('-\n')
+    driver.sleep(5)
+
+
 def main():
     url = 'https://scrap.tf'
-    url_raffles = f'{url}/raffles'
     driver = Driver(uc=True)
     driver.get(url)
 
@@ -53,64 +102,29 @@ def main():
             print(f'{Color.RED}[!] Error when injecting cookies: '
                   f'{e}{Color.RESET}')
 
-    driver.get(url_raffles)
-
-    raffles_links = []
-
     print(
         f'{Color.GREEN}[+] Collecting all currently active raffles...\n{Color.RESET}')
+    raffles_links = collect_raffle_links(driver)
+
     while True:
-        scroll_to_bottom(driver)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        if "That's all, no more!" in soup.get_text():
-            scroll_to_bottom(driver)
-            break
-
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    raffles_div = soup.find('div', id='raffles-list')
-    if raffles_div:
-        raffles = raffles_div.find_all('div', class_='panel-raffle')
-        raffles_stats = driver.find_element(
-            By.CSS_SELECTOR, '.raffle-list-stat h1').text
-        for raffle in raffles:
-            if not raffle.has_attr('class') or 'raffle-entered' not in raffle['class']:
-                raffle_link = raffle.find('a')['href']
-                raffles_links.append(url + raffle_link)
-
         print(
-            f'{Color.GREEN}[+] Open Raffles Entered: {raffles_stats}\n{Color.RESET}')
-
+            f'{Color.GREEN}[+] Open Raffles Found: {len(raffles_links)}\n{Color.RESET}')
         for link in raffles_links:
-            driver.get(link)
-            raffle_name = driver.find_element(
-                By.CSS_SELECTOR, 'h3.subtitle').text
+            enter_raffle(driver, link)
 
-            print(f'{Color.YELLOW}[>] Acessing the raffle:{Color.RESET} '
-                  f'{raffle_name}')
-            print(f'{Color.YELLOW}[>] Link:{Color.RESET} {link}')
+        print(f'{Color.BLUE}[*] Searching for new raffles...')
+        new_raffles_links = collect_raffle_links(driver)
 
-            try:
-                raffle_enter_button = driver.find_element(
-                    By.XPATH, "(//button[contains(text(),'Enter Raffle')])[2]")
+        if new_raffles_links:
+            print(f'[!] New raffles were found!{Color.RESET}\n')
+            raffles_links = new_raffles_links
+            continue
+        break
 
-                driver.execute_script(
-                    "arguments[0].click();", raffle_enter_button)
-                print(
-                    f'{Color.GREEN}[+] Successfully entered the raffle!{Color.RESET}')
-            except Exception as e:
-                print(
-                    f'{Color.RED}[-] Failed to enter the raffle: {e}{Color.RESET}')
-
-            print(f'{Color.YELLOW}[>] Going to next...\n{Color.RESET}')
-            print('-\n')
-            driver.sleep(5)
-
-        driver.get(url_raffles)
-        raffles_stats = driver.find_element(
-            By.CSS_SELECTOR, '.raffle-list-stat h1').text
-
-        print(f'{Color.GREEN}[+] Successfully entered all raffles!')
-        print(f'[+] Open Raffles Entered: {raffles_stats}\n{Color.RESET}')
+    raffles_stats = driver.find_element(
+        By.CSS_SELECTOR, '.raffle-list-stat h1').text
+    print(f'{Color.GREEN}[+] Successfully entered all raffles!')
+    print(f'[+] Open Raffles Entered: {raffles_stats}\n{Color.RESET}')
 
     driver.quit()
 
